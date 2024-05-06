9adfcc3aaca224c9f4c528095ec73c04572151b4
AbzMijon <egorwebdev8@gmail.com>
2024-05-06 21:29:45 +0300

    asd

--- src/components/RegistrationPage/RegistrationPage.jsx
+++ src/components/RegistrationPage/RegistrationPage.jsx
@@ -1,50 +1,61 @@
 import { useEffect, useState } from 'react';
 import styles from './RegistrationPage.module.css';
+import axios from 'axios';
 
 function RegistrationPage() {
 
-    useEffect(() => {
-        localStorage.setItem('users', JSON.stringify([
-            {
-                role: 'user',
-                email: 'user@gmail.com',
-                password: 'sosi'
-            },
-            {
-                role: 'admin',
-                email: 'admin@gmail.com',
-                password: 'sosi'
-            }
-        ]))
-    }, [])
-
     const [mail, setMail] = useState('');
+    const [name, setName] = useState('');
     const [password, setPassword] = useState('');
     const [error, setError] = useState('');
 
-    const onSubmit = () => {
-        setError('');
-        const users = JSON.parse(localStorage.getItem('users'));
-        const isExistUser = users?.find((user) => user.email === mail);
-        if(isExistUser) {
-            const isRightpass = isExistUser?.password === password;
-            if(isRightpass) {
-                localStorage.setItem('isAuth', 'true');
-            }   else {
-                setError('Пароль неверный');
-            }
-        }   else {
-            setError('Не существующая почта');
-        }
+    const onSubmitRegistration = () => {
+        axios.post('YOUR URL', {
+            name: name,
+            email: email,
+            password: password,
+        }).then((res) => {
+            localStorage.setItem('beererToken', res.data);
+        }).catch((err) => {
+            setError(err.detail.message);
+        })
+        setMail('');
+        setName('');
+        setPassword('');
+    }
+
+    const onSubmitLogin = () => {
+        axios.post('YOUR URL', {
+            name: name,
+            email: email,
+            password: password,
+        }).then((res) => {
+            localStorage.setItem('beererToken', res.data);
+        }).catch((err) => {
+            setError(err.detail.message);
+        })
+        setMail('');
+        setName('');
+        setPassword('');
     }
 
     return (
         <div className={styles.RegistrationPage}>
             <h2>Регистрация</h2>
             <div className={styles.RegistrationPage__form}>
+                <input type='text' value={name} onChange={(e) => setName(e.target.value)} />
                 <input type='email' value={mail} onChange={(e) => setMail(e.target.value)} />
                 <input type='password' value={password} onChange={(e) => setPassword(e.target.value)} />
-                <button onClick={onSubmit}>Send</button>
+                <button onClick={onSubmitRegistration}>Send</button>
+            </div>
+            {error ? <p className={styles.RegistrationPage__error}>{error}</p> : null}
+
+            <h2>Войти в аккаунт</h2>
+            <div className={styles.RegistrationPage__form}>
+                <input type='text' value={name} onChange={(e) => setName(e.target.value)} />
+                <input type='email' value={mail} onChange={(e) => setMail(e.target.value)} />
+                <input type='password' value={password} onChange={(e) => setPassword(e.target.value)} />
+                <button onClick={onSubmitLogin}>Send</button>
             </div>
             {error ? <p className={styles.RegistrationPage__error}>{error}</p> : null}
         </div>
