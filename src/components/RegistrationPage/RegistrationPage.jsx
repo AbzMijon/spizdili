3d3ee7e8c17727b304612c9229a17a1dc3a20267
AbzMijon <egorwebdev8@gmail.com>
2024-05-06 21:32:27 +0300

    asd

--- src/components/RegistrationPage/RegistrationPage.jsx
+++ src/components/RegistrationPage/RegistrationPage.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import styles from './RegistrationPage.module.css';
 import axios from 'axios';
 
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -22,7 +22,7 @@ const Header = () => {
           <div className={styles.image_holder}>
             <img src={logo} alt="Logo" />
           </div>
-          Strengthy
+          Life and sport
         </div>
         <ul className={active ? `${styles.activeList}` : ""}>
           {headerLinksData.map((link) => {
